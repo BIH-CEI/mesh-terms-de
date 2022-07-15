780583c034f4e815168ef0e8b3f122cf31af41fc
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-07-15 15:27:53 +0200

    log output

--- mesh-terms/import.py
+++ mesh-terms/import.py
@@ -16,12 +16,15 @@ logger = logging.getLogger(__name__)
 
 
 def import_definitions(url: str, force_download: bool = False):
-    data_dir = pathlib.Path(".") / "data"
+    data_dir = pathlib.Path("data")
 
     if not data_dir.exists() or force_download:
+        logger.info("download new terms...")
         download_terms(url)
+        logger.info("done")
 
     tables = {}
+    logger.info("import tables from files...")
     for file in data_dir.glob("**/*.txt"):
         mapped = FILE_MAP[file.stem]
         try:
@@ -29,11 +32,18 @@ def import_definitions(url: str, force_download: bool = False):
         except KeyError as e:
             logger.error(f"unknown file name {file.stem}: {e}")
             continue
+    logger.info("done")
 
+    logger.info("write tables to database...")
     write_to_db(tables)
+    logger.info("done")
 
 
 if __name__ == "__main__":
     from urls import MESH_DE_2022
+
+    FORMAT = "%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s"
+
+    logging.basicConfig(level=logging.INFO, format=FORMAT)
 
     import_definitions(url=MESH_DE_2022)
