9d92361bed9d9731860333dad1e9ae445c555402
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-07-15 13:01:52 +0200

    correct encoding, quotechar and handle empty trailing columns

--- mesh-terms/convert.py
+++ mesh-terms/convert.py
@@ -18,12 +18,18 @@ def file_to_dataframe(file: str | pathlib.Path, columns: List[str]) -> pd.DataFr
         Dataframe:  With the file's contents
     """
 
-    df = pd.read_csv(file, header=0, sep=";")
+    df = pd.read_csv(file, header=0, quotechar="|", sep=";", encoding="latin1")
 
     # Remove tailoring empty column(s) as separators are used in a strange way
     df.dropna(axis=1, how="all", inplace=True)
 
+    # Replace `NaN` with `None` for easier handling
+    df.where(pd.notnull(df), None, inplace=True)
+
     # Set headers
     df.columns = columns
+
+    if "Id" in columns:
+        df.set_index("Id", inplace=True)
 
     return df
