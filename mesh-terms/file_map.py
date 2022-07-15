8db2fb24b13d638a84a405ab02458badd0162222
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-07-15 14:36:44 +0200

    Define list of column names for each file

--- mesh-terms/file_map.py
+++ mesh-terms/file_map.py
@@ -5,16 +5,33 @@ from typing import List
 @dataclass(kw_only=True, slots=True)
 class FieldMapEntry:
     name: str
+    columns: List[str]
 
 
 FILE_MAP = {
-    "ANN": FieldMapEntry(name="Annotations"),
-    "CC": FieldMapEntry(name="ClassificationCodes"),
-    "ET": FieldMapEntry(name="EntryTerms"),
-    "ETD": FieldMapEntry(name="EntryTermsGerman"),
-    "ETE": FieldMapEntry(name="EntryTermsEnglish"),
-    "MH": FieldMapEntry(name="MainHeadings"),
-    "QF": FieldMapEntry(name="Subheadings"),
-    "RT": FieldMapEntry(name="RelatedTerms"),
-    "SCN": FieldMapEntry(name="ScopeNotesEnglish"),
+    "ANN": FieldMapEntry(name="Annotations", columns=["MainHeadingsId", "Annotation"]),
+    "CC": FieldMapEntry(
+        name="ClassificationCodes", columns=["Classification", "MainHeadingsId"]
+    ),
+    "ET": FieldMapEntry(
+        name="EntryTerms",
+        columns=["DescriptionGerman", "DescriptionEnglish", "MainHeadingsId"],
+    ),
+    "ETD": FieldMapEntry(
+        name="EntryTermsGerman", columns=["Description", "MainHeadingsId"]
+    ),
+    "ETE": FieldMapEntry(
+        name="EntryTermsEnglish", columns=["Description", "MainHeadingsId"]
+    ),
+    "MH": FieldMapEntry(
+        name="MainHeadings",
+        columns=["Id", "DescriptionGerman", "DescriptionEnglish", "SubheadingIds"],
+    ),
+    "QF": FieldMapEntry(
+        name="Subheadings", columns=["Id", "DescriptionGerman", "DescriptionEnglish"]
+    ),
+    "RT": FieldMapEntry(name="RelatedTerms", columns=["First", "Second"]),
+    "SCN": FieldMapEntry(
+        name="ScopeNotesEnglish", columns=["MainHeadingId", "ScopeNote"]
+    ),
 }
