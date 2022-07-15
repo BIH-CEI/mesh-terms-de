ce8c7f29cb1ca188c245e5f97467b76333867080
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-07-15 11:50:51 +0200

    convert csv file (contents) to a dataframe

---
+++ mesh-terms/convert.py
@@ -0,0 +1,29 @@
+import pathlib
+from typing import List
+
+import pandas as pd
+
+
+def file_to_dataframe(file: str | pathlib.Path, columns: List[str]) -> pd.DataFrame:
+    """
+    Read a CSV file to a `Dataframe`
+
+    attr
+    ---
+        file (str | Path):      File to read
+        columns (List[str]):    Names to set for the columns
+
+    returns
+    ---
+        Dataframe:  With the file's contents
+    """
+
+    df = pd.read_csv(file, header=0, sep=";")
+
+    # Remove tailoring empty column(s) as separators are used in a strange way
+    df.dropna(axis=1, how="all", inplace=True)
+
+    # Set headers
+    df.columns = columns
+
+    return df
